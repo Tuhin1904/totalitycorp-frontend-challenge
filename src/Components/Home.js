0c3db04bf465b944711be35af8df13071176fc08
Tuhin Ghosh <iamtuhinn@gmail.com>
2023-09-21 04:03:05 +0530

    home decoration

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -20,22 +20,22 @@ function Home() {
     <div className="imgDiv">
     <img className="bannerImg" src={banner} alt="" />
     </div>
-<hr />
+
     <div className="rows">
-        <div className="rowItem"><Products title="Apple:Macbook pro" price="150" image={laptop} rating='⭐⭐⭐⭐⭐'/></div>
+        <div className="rowItem"><Products title="Mi Buds" price="22" image={buds} rating='⭐⭐⭐'/></div>
         <div className="rowItem"><Products title="iPhone" price="100" image={iphone} rating='⭐⭐⭐⭐'/></div>
         <div className="rowItem"><Products title="Smart Watch" price="50" image={smartWatch} rating='⭐⭐⭐⭐'/></div> 
         <div className="rowItem"><Products title="Headphone" price="10" image={headphone} rating='⭐⭐⭐'/></div>
     </div>
     
     <div className="rows">
-        <div className="rowItem"><Products title="Mi Buds" price="22" image={buds} rating='⭐⭐⭐'/></div>
-        <div className="rowItem"><Products title="60W Bulb " price="30" image={bulb} rating='⭐⭐'/></div>
+        <div className="rowItem"><Products title="Apple:Macbook pro" price="150" image={laptop} rating='⭐⭐⭐⭐⭐'/></div> 
+        <div className="rowItem"><Products title="Notebook set " price="30" image={notebook} rating='⭐⭐'/></div>
         <div className="rowItem"><Products title="AA Battery set" price="6" image={battery} rating='⭐⭐⭐'/></div>
     </div>
     
     <div className="rows">
-        <div className="rowItem"><Products title="Notebook set" price="8" image={notebook} rating='⭐⭐⭐⭐⭐'/></div>
+        <div className="rowItem"><Products title="60W Bulb" price="8" image={bulb} rating='⭐⭐⭐⭐⭐'/></div>
         <div className="rowItem"><Products title="Glycerine soap" price="10" image={soap1} rating='⭐⭐⭐⭐'/></div>
         <div className="rowItem"><Products title="Bag Pack" price="15" image={bag1} rating='⭐⭐⭐'/></div> 
         <div className="rowItem"><Products title="Apple:Macbook pro" price="10" image={pillow1} rating='⭐⭐⭐⭐⭐'/></div> 
